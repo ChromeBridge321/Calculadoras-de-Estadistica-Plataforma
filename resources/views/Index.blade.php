316d1325e1f11c3945dbed428f8d3ed112888e9a
David Garcia Jeronimo <davidgarcia2809@gmail.com>
2025-09-03 13:43:14 -0600

    .

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -12,7 +12,7 @@
         rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <title>Inicio</title>
-    <link rel="icon" type="images/png" href="{{asset('images/logo.png')}}">
+    <link rel="icon" type="images/png" href="https://calculadoras-de-estadistica-plataforma.onrender.com/images/logo.png">
 </head>
 
 <body>
